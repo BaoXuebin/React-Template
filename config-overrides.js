43cf6f456173d26469a168b6e57823c6beb634a2
包学斌 <269203@sh.lianjia.com>
2018-07-09 11:19:15 +0800

    add antd

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+    config = injectBabelPlugin(
+        ['import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true
+        }], config);
+    return config;
+};
